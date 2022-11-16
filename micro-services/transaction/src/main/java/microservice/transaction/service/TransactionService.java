7c6a8433994bd4f11d597510d2f102a7aaaf9fb1
ClementBourdier <clement.bourdier@orange.fr>
2022-11-16 14:49:14 +0100

    Ajout des fonctions dans le service transaction

--- micro-services/transaction/src/main/java/microservice/transaction/service/TransactionService.java
+++ micro-services/transaction/src/main/java/microservice/transaction/service/TransactionService.java
@@ -1,62 +1,102 @@
 package microservice.transaction.service;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Service;
 
 import microservice.transaction.entity.Transaction;
+import microservice.transaction.entity.TransactionAction;
 import microservice.transaction.repository.TransactionRepository;
+import microservice.transaction.rest.card.CardDTO;
+import microservice.transaction.rest.card.CardRestClient;
+import microservice.transaction.rest.user.UserDTO;
+import microservice.transaction.rest.user.UserRestClient;
 
 @Service
 public class TransactionService {
-	//private final CardModelService cardService;
-	//private final UserService userService;
+	private CardRestClient cardService = new CardRestClient();
+	private UserRestClient userService= new UserRestClient();
 	private final TransactionRepository storeRepository;
 
-	public TransactionService(/*CardModelService cardService, UserService userService,*/ TransactionRepository storeRepository) {
-		//this.cardService = cardService;
-		//this.userService = userService;
+	public TransactionService(CardRestClient cardService,  UserRestClient userService,TransactionRepository storeRepository) {
+		this.cardService = cardService;
+		this.userService = userService;
 		this.storeRepository = storeRepository;
 	}
 
-	public boolean buyCard(Integer user_id, Integer card_id) {
-		/*Optional<UserModel> u_option = userService.getUser(user_id);
-		Optional<CardModel> c_option = cardService.getCard(card_id);
+	public HttpServletResponse buyCard(Integer user_id, Integer card_id, HttpServletResponse response) {
+		Optional<UserDTO> u_option = userService.getUserById(user_id);
+		Optional<CardDTO> c_option = cardService.getCardbById(card_id);
 		if (!u_option.isPresent() || !c_option.isPresent()) {
-			return false;
+			try {
+				response.sendError(HttpServletResponse.SC_NOT_FOUND, "User or Card not found");
+				return response;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return response;
+			}
+
 		}
-		UserModel u = u_option.get();
-		CardModel c = c_option.get();
-		if (u.getAccount() > c.getPrice()) {
-			u.addCard(c);
-			u.setAccount(u.getAccount() - c.getPrice());
+		UserDTO u = u_option.get();
+		CardDTO c = c_option.get();
+		if (u.getMoney() > c.getPrice()) {
+			
+			// Mise à jour de l'argent de l'acheteur
+			u.setMoney(u.getMoney() - c.getPrice());
 			userService.updateUser(u);
-			StoreTransaction sT = new StoreTransaction(user_id, card_id, StoreAction.BUY);
+			// Mise à jour de l'id_user de la card
+			c.setId_user(u.getId_user());
+			cardService.updateCard(c);
+			// Création d'un transaction de type BUY
+			Transaction sT = new Transaction(user_id, card_id, TransactionAction.BUY);
 			storeRepository.save(sT);
-			return true;
+			return null;
 		} else {
-			return false;
-		}*/
-		return true;
+			try {
+				response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Not enough money");
+				return response;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return response;
+			}
+
+
+		}
 	}
 
-	public boolean sellCard(Integer user_id, Integer card_id) {
-		/*Optional<UserModel> u_option = userService.getUser(user_id);
-		Optional<CardModel> c_option = cardService.getCard(card_id);
+	public HttpServletResponse sellCard(Integer user_id, Integer card_id, HttpServletResponse response) {
+		Optional<UserDTO> u_option = userService.getUserById(user_id);
+		Optional<CardDTO> c_option = cardService.getCardbById(card_id);
 		if (!u_option.isPresent() || !c_option.isPresent()) {
-			return false;
+			try {
+				response.sendError(HttpServletResponse.SC_NOT_FOUND, "User or Card not found");
+				return response;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return response;
+			}
 		}
-		UserModel u = u_option.get();
-		CardModel c = c_option.get();
+		UserDTO u = u_option.get();
+		CardDTO c = c_option.get();
 
-		c.setUser(null);
+		// Mise à null de l'id_user de la Card
+		c.setId_user(null);
 		cardService.updateCard(c);
-		u.setAccount(u.getAccount() + c.computePrice());
+		
+		// Mise à jour de l'argent de l'utilisateur
+		u.setMoney(u.getMoney() + c.getPrice());
 		userService.updateUser(u);
-		StoreTransaction sT = new StoreTransaction(user_id, card_id, StoreAction.SELL);
-		storeRepository.save(sT);*/
-		return true;
+		
+		// Création d'un transaction de type SELL
+		Transaction sT = new Transaction(user_id, card_id, TransactionAction.SELL);
+		storeRepository.save(sT);
+		
+		return null;
 	}
 
 	public List<Transaction> getAllTransactions() {
